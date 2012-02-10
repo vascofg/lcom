67f23cdb20373ae1785ba0a014f571fa59f73993
ei10054 <ei10054@f72dc856-e8bf-428d-9587-ec951c1e4686>
2012-02-10 20:58:42 +0000

    git-svn-id: https://svn.fe.up.pt/repos/batalha-naval-minix@34 f72dc856-e8bf-428d-9587-ec951c1e4686

--- interface.h
+++
@@ -1,9 +0,0 @@
-/********************************
- *      INTERFACE FUNCTIONS
- ********************************/
-int i_drawMatrix();
-int i_sideBar();
-int i_drawHit(int x, int y, int hit);
-int i_printNumber(int n, int x, int y);
-int i_readXY(int *x, int *y);
-int menu();
